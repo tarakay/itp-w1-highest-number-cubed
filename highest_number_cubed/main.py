f364d31cb4b9df980b9076e8ce6b1651440512e8
Tara Streng <strengtk@gmail.com>
2017-02-17 02:19:13 +0000

    Initial commit

--- highest_number_cubed/main.py
+++ highest_number_cubed/main.py
@@ -1,5 +1,9 @@
 """This is the entry point of the program."""
-
-
 def highest_number_cubed(limit):
-    pass
+    y=0
+    for i in range(limit):
+        while (y**3) <= limit:
+            y += 1
+    return (y -1) 
+
+
